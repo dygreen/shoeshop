3ea89d75abcefc77c907bcecd602c3d004e83e66
dygreen <95523625+dygreen@users.noreply.github.com>
2022-05-21 16:44:14 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
@@ -73,5 +74,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
 
 reportWebVitals();
